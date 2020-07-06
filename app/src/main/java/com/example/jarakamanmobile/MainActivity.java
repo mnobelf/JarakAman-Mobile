b70c478e373632cca68446f7c9bbbaf26436e682
Nobelf <13517042@std.stei.itb.ac.id>
2020-07-07 01:49:41 +0700

    menyelesaikan tampilan beranda

---
+++ app/src/main/java/com/example/jarakamanmobile/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.jarakamanmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+}
